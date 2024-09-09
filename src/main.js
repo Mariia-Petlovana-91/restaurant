0813340853940566d46dcf118e208d1eea88d225
Mariia <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2024-09-09 22:11:23 +0200

    add js files

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import "./js/header";
+import "./js/hero";
+import "./js/principle";
+import "./js/about";
+import "./js/oder";
+import "./js/dishes";
+import "./js/menu";
+import "./js/reviews";
+import "./js/gallery";
+import "./js/cooks";
+import "./js/footer";
+import "./js/form-reviews";
+import "./js/form-order";
